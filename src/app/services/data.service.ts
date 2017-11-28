0fc07957d764c2d06d065c0a8ff14548e9655efa
jonahpelfrey <jsp916@gmail.com>
2017-11-28 10:31:55 -0600

    added dataservice function and model interfaces

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@angular/core';
 
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+
+import { Artist, Volunteer, Buyer, Order } from '../models/models';
+
 @Injectable()
 export class DataService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getArtists() {
+  	this.http.get<Artist[]>('https://afmws.herokuapp.com/api/artists').subscribe(res => {
+  		console.log(res);
+  	});
+  }
 
 }
