3414be198649636f9da10881c99042af576670fd
jonahpelfrey <jsp916@gmail.com>
2017-11-28 21:46:13 -0600

    data service functions: :

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -13,6 +13,18 @@ export class DataService {
 	private artists: BehaviorSubject<Artist[]> = new BehaviorSubject([]);
 	public readonly artistsObs: Observable<Artist[]> = this.artists.asObservable();
 
+	private buyerList: Buyer[];
+	private buyers: BehaviorSubject<Buyer[]> = new BehaviorSubject([]);
+	public readonly buyersObs: Observable<Buyer[]> = this.buyers.asObservable();
+
+	private volunteerList: Volunteer[];
+	private volunteers: BehaviorSubject<Volunteer[]> = new BehaviorSubject([]);
+	public readonly volunteersObs: Observable<Volunteer[]> = this.volunteers.asObservable();
+
+	private orderList: Order[];
+	private orders: BehaviorSubject<Order[]> = new BehaviorSubject([]);
+	public readonly ordersObs: Observable<Order[]> = this.orders.asObservable();
+
 	constructor(private http: HttpClient) {}
 
 	addArtist(artist: Artist) {
@@ -27,6 +39,27 @@ export class DataService {
 			this.artistList = res;
 			this.artists.next(this.artistList);
 		}, err => console.log(err));
+  	}
+
+  	getBuyers() {
+  		this.http.get<Buyer[]>('https://afmws.herokuapp.com/api/buyers').subscribe(res => {
+  			this.buyerList = res;
+  			this.buyers.next(this.buyerList);
+  		}, err => console.log(err));
+  	}
+
+  	getVolunteers() {
+  		this.http.get<Volunteer[]>('https://afmws.herokuapp.com/api/volunteers').subscribe(res => {
+  			this.volunteerList = res;
+  			this.volunteers.next(this.volunteerList);
+  		}, err => console.log(err)):
+  	}
+
+  	getOrders() {
+  		this.http.get<Order[]>('https://afmws.herokuapp.com/api/orders').subscribe(res => {
+  			this.orderList = res;
+  			this.orders.next(this.orderList);
+  		}, err => console.log(err));
   	}
 
 }
