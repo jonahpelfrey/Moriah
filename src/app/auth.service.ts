e2a01f4c336a0a476ae3e4986a9486ad2288e893
jonahpelfrey <jsp916@gmail.com>
2017-12-20 13:37:36 -0600

    login html

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,11 +1,26 @@
 import { Injectable } from '@angular/core';
 
 import { Roles } from './models/roles';
+import { Volunteer } from './models/models';
 
 @Injectable()
 export class AuthService {
 
-	
+	worker: Volunteer;
+
 	constructor() { }
+
+	login(username: string, password: string){
+
+		//Hash password
+
+		//Send username and password to backend to validate login
+
+		//If successful, return true
+	}
+
+	logout(){
+
+	}
 
 }
