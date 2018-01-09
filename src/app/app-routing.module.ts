6215c7ae862db7b7c8f66027c1288b05046b331a
jonahpelfrey <jsp916@gmail.com>
2018-01-09 11:28:31 -0600

    added routes for register, login, and placing orders

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { OrderFormComponent } from './order-form/order-form.component';
+
+const routes: Routes = [
+	{ path: '', redirectTo: 'login', pathMatch: 'full' },
+	{ path: 'login', component: LoginComponent },
+	{ path: 'signup', component: SignupComponent },
+	{ path: 'orders', component: OrderFormComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
